ecc96cd5409c9d0ca2cd1cf51f3692369b7735ba
M-RB3 <rabea9220@gmail.com>
2024-05-08 15:26:53 +0400

    setup test

---
+++ playwright-tests/tests/donate.spec.js
@@ -0,0 +1,42 @@
+/**
+ * donate modal
+ */
+import { test, expect } from "@playwright/test";
+import { ROOT_SRC, DEFAULT_PROJECT_ID } from "../util/constants";
+
+test("should open donate modal", async ({ page }) => {
+  test.setTimeout(120000); // 2 minutes... we want to improve performance
+  await page.goto(`${ROOT_SRC}?tab=project&projectId=${DEFAULT_PROJECT_ID}`);
+  await page.getByRole("button", { name: "Donate" }).click();
+  expect(await page.isVisible("text=Donate to")).toBeTruthy();
+});
+
+test.skip("project with no active pot should donate direct with correct amount", async ({ page }) => {
+  test.setTimeout(120000); // 2 minutes... we want to improve performance
+
+  // go to project page (PotLock project)
+  await page.goto(`${ROOT_SRC}?tab=project&projectId=${DEFAULT_PROJECT_ID}`);
+
+  // click donate button
+  await page.getByRole("button", { name: "Donate" }).click();
+
+  // wait for modal to appear
+  expect(await page.isVisible("text=Donate to")).toBeTruthy();
+
+  // input amount
+  await page.fill("input[name=amount]", "1");
+
+  await page.getByRole("button", { name: "Proceed to donate" }).click();
+
+  // Confirm Donation
+  await page.getByRole("button", { name: "Confirm donation" }).click();
+
+  // Confirmation modal should be visible
+  const transactionObj = JSON.parse(await page.locator("div.modal-body code").innerText());
+  // check if transaction object is correct
+  expect(transactionObj).toMatchObject({
+    bypass_protocol_fee: false,
+    message: "",
+    recipient_id: DEFAULT_PROJECT_ID,
+  });
+});
---
+++ playwright-tests/tests/pot.spec.js
@@ -0,0 +1,25 @@
+/**
+ * ?tab=pot
+ */
+import { test, expect } from "@playwright/test";
+import { ROOT_SRC, DEFAULT_POT_ID } from "../util/constants";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(`${ROOT_SRC}?tab=pot&potId=${DEFAULT_POT_ID}&nav=projects`);
+});
+
+test("clicking project card should go to project page", async ({ page }) => {
+  test.setTimeout(120000); // 1 minute... we want to improve performance
+
+  const projectSearchBar = await page.getByPlaceholder("Search projects");
+
+  await projectSearchBar.fill("magic");
+
+  const projectCard = page.getByText("MagicBuild");
+
+  await Promise.all([projectCard.click(), page.waitForLoadState("load")]);
+
+  const projectUrl = page.url();
+
+  expect(projectUrl).toContain("?tab=project&projectId=magicbuild");
+});
