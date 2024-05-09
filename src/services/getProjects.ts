c3fb7ea7dcf3078bd6a7bb037809af6c0234d4e2
Wenderson Pires <wendersonpdas@gmail.com>
2024-05-09 14:46:36 -0300

    fix projects page performance; shuffle project cards

--- src/services/getProjects.ts
+++ src/services/getProjects.ts
@@ -1,4 +1,14 @@
+import { Storage } from "alem";
 import ListsSDK from "@app/SDK/lists";
+import { Project } from "@app/types";
+
+const PROJECTS_STORAGE_KEY = "previous-projects";
+
+type Data = {
+  allProjects: Project[];
+  approvedProjects: Project[];
+  featuredProjects: Project[];
+};
 
 /**
  * Get current projects.
@@ -8,7 +18,7 @@ const getProjects = () => {
   const allProjects = ListsSDK.getRegistrations();
 
   if (!allProjects) {
-    return { allProjects: [], featuredProjects: [] };
+    return { allProjects: [], approvedProjects: [], featuredProjects: [] } as Data;
   }
 
   allProjects.sort((a: any, b: any) => b.submitted_ms - a.submitted_ms);
@@ -19,7 +29,15 @@ const getProjects = () => {
 
   const approvedProjects = allProjects.filter((project: any) => project.status === "Approved");
 
-  return { allProjects, approvedProjects, featuredProjects };
+  const previousData = Storage.get(PROJECTS_STORAGE_KEY);
+
+  if (previousData) {
+    return previousData as Data;
+  }
+
+  Storage.set(PROJECTS_STORAGE_KEY, { allProjects, approvedProjects, featuredProjects });
+
+  return { allProjects, approvedProjects, featuredProjects } as Data;
 };
 
 export default getProjects;
