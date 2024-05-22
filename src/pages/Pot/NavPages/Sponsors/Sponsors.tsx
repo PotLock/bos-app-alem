7d569c648f04d032176cea18844eb2ad67e934a7
M-RB3 <rabea9220@gmail.com>
2024-05-22 16:35:08 +0400

    update sponsors

--- src/pages/Pot/NavPages/Sponsors/Sponsors.tsx
+++ src/pages/Pot/NavPages/Sponsors/Sponsors.tsx
@@ -1,50 +1,59 @@
-import { State, state, useParams } from "alem";
-import PotSDK from "@app/SDK/pot";
-import { PotDetail } from "@app/types";
+import { useEffect, useParams, useState } from "alem";
+import { getConfig, getSponsorships } from "@app/services/getPotData";
+import { PotDetail, PotDonation } from "@app/types";
 import SponsorsBoard from "../../components/SponsorsBoard/SponsorsBoard";
 import SponsorsTable from "../../components/SponsorsTable/SponsorsTable";
 import { Container, TableContainer } from "./styles";
 
-const Sponsors = ({ potDetail }: { potDetail: PotDetail }) => {
+const Sponsors = () => {
   const { potId } = useParams();
 
-  let sponsorshipDonations = PotSDK.getMatchingPoolDonations(potId);
+  const [sponsorshipDonations, setSponsorshipDonations] = useState<null | PotDonation[]>(null);
+  const [potDetail, setPotDetail] = useState<null | PotDetail>(null);
 
-  State.init({
-    sponsorshipDonations: null,
-  });
+  useEffect(() => {
+    if (!potDetail) {
+      getConfig({
+        potId,
+        updateState: setPotDetail,
+      });
+    }
+    if (!sponsorshipDonations && potDetail) {
+      getSponsorships({
+        potId,
+        updateState: (donations) => {
+          let sponsorshipDonations = donations.reduce((accumulator: any, currentDonation: any) => {
+            accumulator[currentDonation.donor_id] = {
+              amount: accumulator[currentDonation.donor_id].amount || 0 + currentDonation.net_amount,
+              ...currentDonation,
+            };
+            return accumulator;
+          }, {});
 
-  if (sponsorshipDonations && !state.sponsorshipDonations) {
-    // accumulate donations for each address
-    sponsorshipDonations = sponsorshipDonations.reduce((accumulator: any, currentDonation: any) => {
-      accumulator[currentDonation.donor_id] = {
-        amount: accumulator[currentDonation.donor_id].amount || 0 + currentDonation.net_amount,
-        ...currentDonation,
-      };
-      return accumulator;
-    }, {});
+          // add % share of total to each donation
+          const total = parseFloat(potDetail.matching_pool_balance);
 
-    // add % share of total to each donation
-    const total = parseFloat(potDetail.matching_pool_balance);
+          sponsorshipDonations = Object.values(sponsorshipDonations).sort((a: any, b: any) => b.amount - a.amount);
+          sponsorshipDonations = sponsorshipDonations.map((donation: any) => {
+            return {
+              ...donation,
+              percentage_share: ((donation.amount / total) * 100).toFixed(2).replace(/[.,]00$/, ""),
+            };
+          });
+          setSponsorshipDonations(sponsorshipDonations);
+        },
+      });
+    }
+  }, [potDetail]);
 
-    sponsorshipDonations = Object.values(sponsorshipDonations).sort((a: any, b: any) => b.amount - a.amount);
-    sponsorshipDonations = sponsorshipDonations.map((donation: any) => {
-      return {
-        ...donation,
-        percentage_share: ((donation.amount / total) * 100).toFixed(2).replace(/[.,]00$/, ""),
-      };
-    });
-    State.update({ sponsorshipDonations });
-  }
-
-  if (!state.sponsorshipDonations) return <div className="spinner-border text-secondary" role="status" />;
+  if (!sponsorshipDonations) return <div className="spinner-border text-secondary" role="status" />;
 
   return (
     <Container>
-      <SponsorsBoard donations={state.sponsorshipDonations.slice(0, 6)} />
+      <SponsorsBoard donations={sponsorshipDonations.slice(0, 6)} />
 
       <TableContainer>
-        <SponsorsTable sponsors={state.sponsorshipDonations} />
+        <SponsorsTable sponsors={sponsorshipDonations} />
       </TableContainer>
     </Container>
   );
