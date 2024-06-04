86b32f98682328f1e15049432d4d121befbdef5c
M-RB3 <rabea9220@gmail.com>
2024-06-04 18:30:12 +0400

    fix donations limit

--- src/services/getPotData.ts
+++ src/services/getPotData.ts
@@ -200,7 +200,7 @@ export const getPayout = ({
 
 // get matched donations
 export const asyncGetPublicDonations = (potDetail: PotDetail, potId: string) => {
-  const limit = 450; // number of donations to fetch per req
+  const limit = 350; // number of donations to fetch per req
 
   const donationsCount = potDetail.public_donations_count;
   const paginations = [...Array(Math.ceil(donationsCount / limit)).keys()];
